21d1ec627446e7d468555df8e12b2c4da5e7d666
vit9696 <vit9696@users.noreply.github.com>
2018-09-30 18:23:35 +0300

    Implement initial version of OcOverflowLib without generic compiler support

---
+++ Library/OcOverflowLib/BitOverflow.c
@@ -0,0 +1,211 @@
+/** @file
+
+OcCryptoLib
+
+Copyright (c) 2018, vit9696
+
+All rights reserved.
+
+This program and the accompanying materials
+are licensed and made available under the terms and conditions of the BSD License
+which accompanies this distribution.  The full text of the license may be found at
+http://opensource.org/licenses/bsd-license.php
+
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#include <Library/OcOverflowLib.h>
+
+BOOLEAN
+(OcOverflowAddU32) (
+  UINT32  A,
+  UINT32  B,
+  UINT32  *Result
+  )
+{
+  while (1) {
+    (VOID) A;
+    (VOID) B;
+    (VOID) Result;
+    /* Unimplemented */
+  }
+  return TRUE;
+}
+
+BOOLEAN
+(OcOverflowSubU32) (
+  UINT32  A,
+  UINT32  B,
+  UINT32  *Result
+  )
+{
+  while (1) {
+    (VOID) A;
+    (VOID) B;
+    (VOID) Result;
+    /* Unimplemented */
+  }
+  return TRUE;
+}
+
+BOOLEAN
+(OcOverflowMulU32) (
+  UINT32  A,
+  UINT32  B,
+  UINT32  *Result
+  )
+{
+  while (1) {
+    (VOID) A;
+    (VOID) B;
+    (VOID) Result;
+    /* Unimplemented */
+  }
+  return TRUE;
+}
+
+BOOLEAN
+(OcOverflowAddS32) (
+  INT32  A,
+  INT32  B,
+  INT32  *Result
+  )
+{
+  while (1) {
+    (VOID) A;
+    (VOID) B;
+    (VOID) Result;
+    /* Unimplemented */
+  }
+  return TRUE;
+}
+
+BOOLEAN
+(OcOverflowSubS32) (
+  INT32  A,
+  INT32  B,
+  INT32  *Result
+  )
+{
+  while (1) {
+    (VOID) A;
+    (VOID) B;
+    (VOID) Result;
+    /* Unimplemented */
+  }
+  return TRUE;
+}
+
+BOOLEAN
+(OcOverflowMulS32) (
+  INT32  A,
+  INT32  B,
+  INT32  *Result
+  )
+{
+  while (1) {
+    (VOID) A;
+    (VOID) B;
+    (VOID) Result;
+    /* Unimplemented */
+  }
+  return TRUE;
+}
+
+BOOLEAN
+(OcOverflowAddU64) (
+  UINT64  A,
+  UINT64  B,
+  UINT64  *Result
+  )
+{
+  while (1) {
+    (VOID) A;
+    (VOID) B;
+    (VOID) Result;
+    /* Unimplemented */
+  }
+  return TRUE;
+}
+
+BOOLEAN
+(OcOverflowSubU64) (
+  UINT64  A,
+  UINT64  B,
+  UINT64  *Result
+  )
+{
+  while (1) {
+    (VOID) A;
+    (VOID) B;
+    (VOID) Result;
+    /* Unimplemented */
+  }
+  return TRUE;
+}
+
+BOOLEAN
+(OcOverflowMulU64) (
+  UINT64  A,
+  UINT64  B,
+  UINT64  *Result
+  )
+{
+  while (1) {
+    (VOID) A;
+    (VOID) B;
+    (VOID) Result;
+    /* Unimplemented */
+  }
+  return TRUE;
+}
+
+BOOLEAN
+(OcOverflowAddS64) (
+  INT64  A,
+  INT64  B,
+  INT64  *Result
+  )
+{
+  while (1) {
+    (VOID) A;
+    (VOID) B;
+    (VOID) Result;
+    /* Unimplemented */
+  }
+  return TRUE;
+}
+
+BOOLEAN
+(OcOverflowSubS64) (
+  INT64  A,
+  INT64  B,
+  INT64  *Result
+  )
+{
+  while (1) {
+    (VOID) A;
+    (VOID) B;
+    (VOID) Result;
+    /* Unimplemented */
+  }
+  return TRUE;
+}
+
+BOOLEAN
+(OcOverflowMulS64) (
+  INT64  A,
+  INT64  B,
+  INT64  *Result
+  )
+{
+  while (1) {
+    (VOID) A;
+    (VOID) B;
+    (VOID) Result;
+    /* Unimplemented */
+  }
+  return TRUE;
+}
